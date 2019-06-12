9aee325674a8ba04d1c9550ada0da89c8b1099f2
natsutsuki <827868421@qq.com>
2019-06-13 00:28:44 +0800

    Initial Commit

---
+++ cool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cool
+//
+//  Created by c.c on 2019/6/13.
+//  Copyright © 2019 c.c. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
